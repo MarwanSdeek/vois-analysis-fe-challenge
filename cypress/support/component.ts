abf0283cd84ee7b26b8998fd0dacf6dee9799b3f
Marwan <marwan.sdeek.web@gmail.com>
2022-08-09 03:23:30 +0200

    cypress - fix typing for mountWithRouter

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -1,41 +1,25 @@
-// ***********************************************************
-// This example support/component.ts is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
+import { mount } from 'cypress/react'
+import { MountOptions, MountReturn } from 'cypress/react'
+import { MemoryRouterProps } from 'react-router-dom'
 
-// Import commands.js using ES2015 syntax:
 import './commands'
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
-
-// should be changed to cypress/react18 to fix warnings for React 18 but it's still in development
-// ref: https://github.com/cypress-io/cypress/issues/23081#issuecomment-1204553702
-import { mount } from 'cypress/react'
-
-// Augment the Cypress namespace to include type definitions for
-// your custom command.
-// Alternatively, can be defined in cypress/support/component.d.ts
-// with a <reference path="./component" /> at the top of your spec.
 declare global {
   namespace Cypress {
     interface Chainable {
       mount: typeof mount
+
+      /**
+       * Mounts a React node
+       * @param component React Node to mount
+       * @param options Additional options to pass into mount
+       */
+      mountWithRouter(
+        component: React.ReactNode,
+        options?: MountOptions & { routerProps?: MemoryRouterProps }
+      ): Cypress.Chainable<MountReturn>;
     }
   }
 }
 
 Cypress.Commands.add('mount', mount)
-
-// Example use:
-// cy.mount(<MyComponent />)
