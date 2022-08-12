141d5790e68cb89b42987f09678789b903fedd27
Marwan <marwan.sdeek.web@gmail.com>
2022-08-12 21:22:26 +0200

    cypress - PageContainer

---
+++ cypress/component/components/basic/PageContainer.cy.tsx
@@ -0,0 +1,22 @@
+import { PageContainer } from '../../../../src/components/basic'
+
+describe('PageContainer', () => {
+	it('mount', () => {
+		cy.mount(<Comp />)
+
+		cy.getByTestId('pageContainer')
+			.should('be.visible')
+			.should('have.css', 'min-height')
+	})
+})
+
+
+const Comp = () => (
+	<PageContainer data-testid="pageContainer" css={STYLE}>
+		<p> A Page Container </p>
+	</PageContainer>
+)
+
+const STYLE = {
+	border: '1px dashed red'
+}
